682685b623167f5c391d9bdc82dfe3bca0b57fe8
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-08-13 02:09:35 +0100

    Front End Active

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-rdukjoz6.us', // the auth0 domain prefix
+    audience: 'udacityCoffee', // the audience set for the auth0 app
+    clientId: 'YsRzg4epeAM7ZPwpx9JMTpZC6P1xpGEi', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
